e937c5fccae394431f5db3c987f56a23abe275fd
Tom Pinton <tom.snak00s@gmail.com>
2026-04-16 12:39:03 +0200

    test fedora

--- ft_nm.c
+++ ft_nm.c
@@ -18,53 +18,53 @@ int main(void)
 		return (0);
 	}
 	fstat(fd, &fdstat);
-	// printf("fstat value : %d\n", st);
-	// printf("st_atime : %ld\n", fdstat.st_atime);
-	// printf("st_blksize : %ld\n", fdstat.st_blksize);
-	// printf("st_blocks : %ld\n", fdstat.st_blocks);
-	// printf("st_ctime : %ld\n", fdstat.st_ctime);
-	// printf("st_dev : %lu\n", fdstat.st_dev);
-	// printf("st_gid : %u\n", fdstat.st_gid);
-	// printf("st_ino : %lu\n", fdstat.st_ino);
-	// printf("st_mode : %u\n", fdstat.st_mode);
-	// printf("st_mtime : %ld\n", fdstat.st_mtime);
-	// printf("st_nlink : %lu\n", fdstat.st_nlink);
-	// printf("st_rdev : %lu\n", fdstat.st_rdev);
-	// printf("st_size : %ld\n", fdstat.st_size);
-	// printf("st_uid : %u\n", fdstat.st_uid);
-	// printf("__glibc_reserved[0] : %ld\n", fdstat.__glibc_reserved[0]);
-	// printf("__glibc_reserved[1] : %ld\n", fdstat.__glibc_reserved[1]);
-	// printf("__glibc_reserved[2] : %ld\n", fdstat.__glibc_reserved[2]);
-	// printf("__pad0 : %d\n", fdstat.__pad0);
 
 	void *map_start = mmap(NULL, fdstat.st_size, PROT_READ, MAP_SHARED, fd, 0);
 
-	printf("%s\n", (char *)map_start);
-
 	Elf64_Ehdr	*header = (Elf64_Ehdr *)map_start;
+
+	char *string = (char *)map_start;
+
+	printf("%1$p : %1$s\n", string + 0x3541);
 
 	// if (memcmp(header->e_ident, ELFMAG, 4) == 0) {
 	// 	printf("ELF valide, %d sections\n", header->e_shnum);
 	// }
 
-	Elf64_Shdr	*sections;
-
-	sections = (Elf64_Shdr *)map_start;
-
-	Elf64_Sym *symtab;
+	Elf64_Shdr	*sections = (Elf64_Shdr *)(map_start + header->e_shoff);
 
 	printf("%d\n", header->e_shnum);
 
+	int strtabIdx = header->e_shstrndx;
+
+	if (header->e_shstrndx >= SHN_LORESERVE)
+		strtabIdx = sections[0].sh_link;
+
+	// Elf64_Sym *symtab = NULL;
+
+	// for (int i = 0; i < header->e_shnum; i++)
+	// {
+	// 	// printf("section %d name : %lx | %s\n", i, test->st_value, (char *)(map_start + sections[i].sh_offset) + sections[i].sh_name);
+	// 	if (sections[i].sh_type == SHT_SYMTAB)
+	// 	{
+	// 		symtab = (Elf64_Sym *)(map_start + sections[i].sh_offset);
+	// 		printf("index symtab : %d\n", i);
+	// 		break;
+	// 	}
+	// }
+
+	Elf64_Sym *strtab = (map_start + sections[strtabIdx].sh_offset);
+
+	Elf64_Sym *data = NULL;
 	for (int i = 0; i < header->e_shnum; i++)
 	{
-		printf("section %d : %u\n", i, sections[i].sh_type);
-		if (sections[i].sh_type == SHT_SYMTAB)
+		printf("section[%d] : %s\n", i,  (char *)strtab + sections[i].sh_name);
+		if (!strcmp((char *)strtab + sections[i].sh_name, ".data"))
 		{
-			symtab = (Elf64_Sym *)((char *)map_start + sections[i].sh_offset);
-			printf("founded\n");
-			break;
+			data = (map_start + sections[i].sh_offset);
 		}
 	}
 
+	printf("%s\n", (char *)strtab + data->st_name);
 	return (0);
 }
